dc269ade72349799c4c59036e4ccc3482de6ffec
yumXD <qkpw99@gmail.com>
2024-04-19 05:02:36 +0000

    Feat: 클라이언트로부터 받은 URI를 분석(#1)

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -90,6 +90,28 @@ void read_requesthdrs(rio_t *rp) {
     return;
 }
 
+int parse_uri(char *uri, char *filename, char *cgiargs) {
+    char *ptr;
+    if (!strstr(uri, "cgi-bin")) {
+        strcpy(cgiargs, "");
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        if (uri[strlen(uri) - 1] == '/')
+            strcat(filename, "home.html");
+        return 1;
+    } else {
+        ptr = index(uri, '?');
+        if (ptr) {
+            strcpy(cgiargs, ptr + 1);
+            *ptr = '\0';
+        } else
+            strcpy(cgiargs, "");
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        return 0;
+    }
+}
+
 int main(int argc, char **argv) {
     int listenfd, connfd;
     char hostname[MAXLINE], port[MAXLINE];
