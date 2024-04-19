51c63ff36ee287d688a61848d08d58ff82a45bd9
yumXD <qkpw99@gmail.com>
2024-04-19 02:35:57 +0000

    Refactor: 코드 정렬하기(#1)

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -9,35 +9,41 @@
 #include "csapp.h"
 
 void doit(int fd);
+
 void read_requesthdrs(rio_t *rp);
+
 int parse_uri(char *uri, char *filename, char *cgiargs);
+
 void serve_static(int fd, char *filename, int filesize);
+
 void get_filetype(char *filename, char *filetype);
+
 void serve_dynamic(int fd, char *filename, char *cgiargs);
+
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
 int main(int argc, char **argv) {
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
-  socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
 
-  /* Check command line args */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
+    /* Check command line args */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
 
-  listenfd = Open_listenfd(argv[1]);
-  while (1) {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr,
-                    &clientlen);  // line:netp:tiny:accept
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
-    printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);   // line:netp:tiny:doit
-    Close(connfd);  // line:netp:tiny:close
-  }
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA * ) & clientaddr,
+                        &clientlen);  // line:netp:tiny:accept
+        Getnameinfo((SA * ) & clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
+                    0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        doit(connfd);   // line:netp:tiny:doit
+        Close(connfd);  // line:netp:tiny:close
+    }
 }
