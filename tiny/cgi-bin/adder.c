ef6e7d41fc4ba7682c3247bab64e7065cd163f23
yumXD <qkpw99@gmail.com>
2024-04-20 03:46:38 +0000

    Feat: 쿼리를 통한 adder가 생성한 동적 컨텐츠를 표시(#9)

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -14,10 +14,8 @@ int main(void) {
     if ((buf = getenv("QUERY_STRING")) != NULL) {
         p = strchr(buf, '&');
         *p = '\0';
-        strcpy(arg1, buf);
-        strcpy(arg2, p + 1);
-        n1 = atoi(arg1);
-        n2 = atoi(arg2);
+        sscanf(buf, "num1=%d", &n1);
+        sscanf(p + 1, "num2=%d", &n2);
     }
 
     /* Make the response body */
