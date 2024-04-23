e19f197d7805b1ba2a013b96b6197469862ea688
yumXD <qkpw99@gmail.com>
2024-04-23 13:45:46 +0000

    Fix: 안튕기게 수정

--- proxy.c
+++ proxy.c
@@ -64,21 +64,20 @@ void doit(int clientfd) {
     }
 
     printf("%s\n", request_buf);
+    rio_writen(serverfd, request_buf, strlen(request_buf));
     Rio_readinitb(&response_rio, serverfd);
-    Rio_writen(serverfd, request_buf, strlen(request_buf)); // 서버에 요청 전송
 
-    /* 서버로부터 응답 받아 클라이언트에 전송 */
     ssize_t n;
     /* 응답 헤더 보내기 */
     while ((n = Rio_readlineb(&response_rio, response_buf, MAX_OBJECT_SIZE)) > 0) {
-        Rio_writen(clientfd, response_buf, n);
+        rio_writen(clientfd, response_buf, n);
         if (!strcmp(response_buf, "\r\n"))
             break;
     }
 
     /* 응답 본문 보내기 */
     while ((n = Rio_readlineb(&response_rio, response_buf, MAX_OBJECT_SIZE)) > 0) {
-        Rio_writen(clientfd, response_buf, n);
+        rio_writen(clientfd, response_buf, n);
     }
 
     Close(serverfd); // 서버 연결 종료
