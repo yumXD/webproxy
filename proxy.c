cd9b96aa667428e082df607e679b822eb182b2cd
yumXD <qkpw99@gmail.com>
2024-04-23 04:36:40 +0000

    Fix: 동영상 지속안되는 버그 수정(#15)

--- proxy.c
+++ proxy.c
@@ -64,12 +64,22 @@ void doit(int clientfd) {
     }
 
     printf("%s\n", request_buf);
+    Rio_readinitb(&response_rio, serverfd);
     Rio_writen(serverfd, request_buf, strlen(request_buf)); // 서버에 요청 전송
 
     /* 서버로부터 응답 받아 클라이언트에 전송 */
     ssize_t n;
-    n = Rio_readn(serverfd, response_buf, MAX_OBJECT_SIZE); // 서버로부터 OBJECT_SIZE 만큼 응답을 읽음
-    Rio_writen(clientfd, response_buf, n); // 클라이언트에게 응답을 전송
+    /* 응답 헤더 보내기 */
+    while ((n = Rio_readlineb(&response_rio, response_buf, MAX_OBJECT_SIZE)) > 0) {
+        Rio_writen(clientfd, response_buf, n);
+        if (!strcmp(response_buf, "\r\n"))
+            break;
+    }
+
+    /* 응답 본문 보내기 */
+    while ((n = Rio_readlineb(&response_rio, response_buf, MAX_OBJECT_SIZE)) > 0) {
+        Rio_writen(clientfd, response_buf, n);
+    }
 
     Close(serverfd); // 서버 연결 종료
 }
